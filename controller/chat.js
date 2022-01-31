ee7ae6f5fb140f382ff63fb14e9b85438a2ed68c
TYRPRO <www.yongrui@gmail.com>
2022-01-31 16:36:22 +0800

    disabled chat temporarily

--- controller/chat.js
+++ controller/chat.js
@@ -2,33 +2,34 @@ const express = require("express");
 const router = express.Router();
 const { Server } = require("socket.io");
 
-var PORT = process.env.PORT;
+// var PORT = process.env.PORT;
+// var INDEX = "/home";
+
+// const server = express()
+// 	.use((req, res) => res.sendFile(INDEX, { root: __dirname }))
+// 	.listen(PORT, () => console.log(`Listening on ${PORT}`));
+
+// const io = new Server({ server });
 
 
-const server = express()
-	.listen(PORT, () => console.log(`Listening on ${PORT}`));
 
-const io = new Server({ server });
+// io.on("connection", (socket) => {
+// 	socket.on("join", (data) => {
+// 		socket.join(data.roomid);
+// 	});
 
-
-
-io.on("connection", (socket) => {
-	socket.on("join", (data) => {
-		socket.join(data.roomid);
-	});
-
-	socket.on("sendmessage", (data) => {
-		console.log(data);
-		if (data.user != undefined) {
-			io.in(data.roomid).emit("receivemessage", data);
-		}
-	});
+// 	socket.on("sendmessage", (data) => {
+// 		console.log(data);
+// 		if (data.user != undefined) {
+// 			io.in(data.roomid).emit("receivemessage", data);
+// 		}
+// 	});
 
 
-	socket.on("leave", (data) => {
-		socket.leave(data.roomid);
-	});
-});
+// 	socket.on("leave", (data) => {
+// 		socket.leave(data.roomid);
+// 	});
+// });
 
 
 module.exports = router;
